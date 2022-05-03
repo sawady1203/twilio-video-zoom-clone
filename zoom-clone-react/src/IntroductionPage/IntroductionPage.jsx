2153c01bc19722ca834aece4e312e7815f614466
sawady1203 <sawady.dev@gmail.com>
2022-05-03 15:04:15 +0900

    create the join buttons

--- zoom-clone-react/src/IntroductionPage/IntroductionPage.jsx
+++ zoom-clone-react/src/IntroductionPage/IntroductionPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./IntroductionPage.css";
 import logo from "../resources/images/logo.png";
+import ConnectingButtons from "./ConnectingButtons";
 
 function IntroductionPage() {
   return (
     <div className="introduction_page_container">
       <div className="introduction_page_panel">
         <img src={logo} alt="logo" className="introduction_page_image" />
+        <ConnectingButtons />
       </div>
     </div>
   );

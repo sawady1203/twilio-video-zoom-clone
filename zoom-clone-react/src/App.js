0c676e11df7922fdbc273baf0a92ff1079204f13
sawady1203 <sawady.dev@gmail.com>
2022-05-03 11:49:16 +0900

    feature/React router

--- zoom-clone-react/src/App.js
+++ zoom-clone-react/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import JoinRoomPage from "./JoinRoomPage/JoinRoomPage";
+import RoomPage from "./RoomPage/RoomPage";
+import IntroductionPage from "./IntroductionPage/IntroductionPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<IntroductionPage />} />
+        <Route path="join-room" element={<JoinRoomPage />} />
+        <Route path="room" element={<RoomPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

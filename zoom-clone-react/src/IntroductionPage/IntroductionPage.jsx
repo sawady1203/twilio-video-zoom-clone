817702395d7c1580e2ef8552752add0a7d7d7ea7
sawady1203 <sawady.dev@gmail.com>
2022-05-03 11:49:48 +0900

    update basic structure and css

--- zoom-clone-react/src/IntroductionPage/IntroductionPage.jsx
+++ zoom-clone-react/src/IntroductionPage/IntroductionPage.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./IntroductionPage.css";
+import logo from "../resources/images/logo.png";
 
 function IntroductionPage() {
-  return <div>IntroductionPage</div>;
+  return (
+    <div className="introduction_page_container">
+      <div className="introduction_page_panel">
+        <img src={logo} alt="logo" className="introduction_page_image" />
+      </div>
+    </div>
+  );
 }
 
 export default IntroductionPage;
